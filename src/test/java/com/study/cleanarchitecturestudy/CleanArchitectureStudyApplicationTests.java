50871a0d3d8200fdf1bb0a76d71af15174042681
ICCHOI <cicblck@gmail.com>
2023-01-03 23:29:20 +0900

    init : 프로젝트 설정

---
+++ src/test/java/com/study/cleanarchitecturestudy/CleanArchitectureStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.cleanarchitecturestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
